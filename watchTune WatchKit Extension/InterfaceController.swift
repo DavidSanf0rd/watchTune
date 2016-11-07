e107002a5a0fec7381ce8574d578c347a3a98bb5
Vitor Muniz <vitormuniz@Vitors-MacBook-Pro.local>
2016-11-07 19:21:59 -0300

    put the pitch detaction logic

--- watchTune WatchKit Extension/InterfaceController.swift
+++ watchTune WatchKit Extension/InterfaceController.swift
@@ -8,20 +8,13 @@
 
 import WatchKit
 import Foundation
-import AudioKit
+
 
 class InterfaceController: WKInterfaceController {
     
     
     @IBOutlet var lblNote: WKInterfaceLabel!
-    
-    var mic: AKMicrophone!
-    var tracker: AKFrequencyTracker!
-    var silence: AKBooster!
-    
-    let noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]//oitava 0
-    let noteNamesWithSharps = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
-    let noteNamesWithFlats = ["C", "D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"]
+   
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -39,34 +32,6 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
-    func updateUI() {
-        if tracker.amplitude > 0.1 {
-            frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
-            
-            var frequency = Float(tracker.frequency)
-            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
-                frequency = frequency / 2.0
-            }
-            while (frequency < Float(noteFrequencies[0])) {
-                frequency = frequency * 2.0
-            }
-            
-            var minDistance: Float = 10000.0
-            var index = 0
-            
-            for i in 0..<noteFrequencies.count {
-                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
-                if (distance < minDistance){
-                    index = i
-                    minDistance = distance
-                }
-            }
-            let octave = Int(log2f(Float(tracker.frequency) / frequency))
-            noteNameWithSharpsLabel.text = "\(noteNamesWithSharps[index])\(octave)"
-            noteNameWithFlatsLabel.text = "\(noteNamesWithFlats[index])\(octave)"
-        }
-        amplitudeLabel.text = String(format: "%0.2f", tracker.amplitude)
-    }
-
+    
     
 }
--- watchTune/ViewController.swift
+++ watchTune/ViewController.swift
@@ -10,17 +10,62 @@ import UIKit
 import AudioKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblNote: UILabel!
+    
+    var mic: AKMicrophone!
+    var tracker: AKFrequencyTracker!
+    var silence: AKBooster!
+    
+    let noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]//oitava 0
+    let noteNames = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        AKSettings.audioInputEnabled = true
+        mic = AKMicrophone()
+        tracker = AKFrequencyTracker.init(mic)
+        silence = AKBooster(tracker, gain: 0)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        AudioKit.output = silence
+        AudioKit.start()
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateUI), userInfo: nil, repeats: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateUI() {
+        if tracker.amplitude > 0.1 {
+            lblNote.text = String(format: "%0.1f", tracker.frequency)
+            
+            var frequency = Float(tracker.frequency)
+            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
+                frequency = frequency / 2.0
+            }
+            while (frequency < Float(noteFrequencies[0])) {
+                frequency = frequency * 2.0
+            }
+            
+            var minDistance: Float = 10000.0
+            var index = 0
+            
+            for i in 0..<noteFrequencies.count {//vai verificando de nota por nota até achar a que tem a menor distancia
+                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
+                if (distance < minDistance){
+                    index = i
+                    minDistance = distance
+                }
+            }
+            let octave = Int(log2f(Float(tracker.frequency) / frequency))
+            lblNote.text = "\(noteNames[index])\(octave)"
+        }
+    }
 
 }
 
