5c64077e8a10762628cea1210aa389f7a86eaf37
trastoan <yurisaboiaf@gmail.com>
2016-11-09 12:06:34 -0300

    Falta mudar umas paradas

--- watchTune/ViewController.swift
+++ watchTune/ViewController.swift
@@ -12,6 +12,7 @@ import AudioKit
 class ViewController: UIViewController {
     
 //    @IBOutlet weak var lblNote: UILabel!
+    let noteNames = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
     
     var colors = [UIColor(red: 63.0/255.0, green: 225.0/255.0, blue: 232.0/255.0, alpha: 1.0),                                                 // Blues
                   UIColor(red: 67.0/255.0, green: 158.0/255.0, blue: 224.0/255.0, alpha: 1.0),
@@ -55,11 +56,71 @@ class ViewController: UIViewController {
 
     func updateUI() {
         if tracker.amplitude > 0.1 {
-           let number = Int(arc4random_uniform(8))
-           self.circles.printCircles(filledCircle: CGFloat(number))
-           self.view.backgroundColor = (colors[number])
-           self.circles.view.backgroundColor = (colors[number])
-           self.picker.view.backgroundColor = (colors[number])
+           
+           let number = MusicUtilities.directionToTune(desireNote: noteNames[Int(picker.lastSelectedItem)], currentFrequency: self.tracker.frequency)
+            switch number {
+            case _ where number <= -0.80:
+                self.circles.printCircles(filledCircle: CGFloat(0))
+                self.view.backgroundColor = (colors[0])
+                self.circles.view.backgroundColor = (colors[0])
+                self.picker.view.backgroundColor = (colors[0])
+            case _ where number <= -0.60 && number >= -0.80:
+                self.circles.printCircles(filledCircle: CGFloat(1))
+                self.view.backgroundColor = (colors[1])
+                self.circles.view.backgroundColor = (colors[1])
+                self.picker.view.backgroundColor = (colors[1])
+            case _ where number <= -0.40 && number >= -0.60:
+                self.circles.printCircles(filledCircle: CGFloat(2))
+                self.view.backgroundColor = (colors[2])
+                self.circles.view.backgroundColor = (colors[2])
+                self.picker.view.backgroundColor = (colors[2])
+            case _ where number <= -0.20 && number >= -0.40:
+                self.circles.printCircles(filledCircle: CGFloat(3))
+                self.view.backgroundColor = (colors[3])
+                self.circles.view.backgroundColor = (colors[3])
+                self.picker.view.backgroundColor = (colors[3])
+            case _ where number < 0 && number >= -0.20:
+                self.circles.printCircles(filledCircle: CGFloat(3))
+                self.view.backgroundColor = (colors[3])
+                self.circles.view.backgroundColor = (colors[3])
+                self.picker.view.backgroundColor = (colors[3])
+            case _ where number == 0:
+                self.circles.printCircles(filledCircle: CGFloat(4))
+                self.view.backgroundColor = (colors[4])
+                self.circles.view.backgroundColor = (colors[4])
+                self.picker.view.backgroundColor = (colors[4])
+            case _ where number > 0 && number < 0.2:
+                self.circles.printCircles(filledCircle: CGFloat(5))
+                self.view.backgroundColor = (colors[5])
+                self.circles.view.backgroundColor = (colors[5])
+                self.picker.view.backgroundColor = (colors[5])
+            case _ where number > 0.2 && number < 0.4:
+                self.circles.printCircles(filledCircle: CGFloat(6))
+                self.view.backgroundColor = (colors[6])
+                self.circles.view.backgroundColor = (colors[6])
+                self.picker.view.backgroundColor = (colors[6])
+            case _ where number > 0.4 && number < 0.6:
+                self.circles.printCircles(filledCircle: CGFloat(7))
+                self.view.backgroundColor = (colors[7])
+                self.circles.view.backgroundColor = (colors[7])
+                self.picker.view.backgroundColor = (colors[7])
+            case _ where number > 0.6 && number < 0.8:
+                self.circles.printCircles(filledCircle: CGFloat(7))
+                self.view.backgroundColor = (colors[7])
+                self.circles.view.backgroundColor = (colors[7])
+                self.picker.view.backgroundColor = (colors[7])
+            case _ where number > 0.8:
+                self.circles.printCircles(filledCircle: CGFloat(8))
+                self.view.backgroundColor = (colors[8])
+                self.circles.view.backgroundColor = (colors[8])
+                self.picker.view.backgroundColor = (colors[8])
+            default:
+                self.circles.printCircles(filledCircle: CGFloat(4))
+                self.view.backgroundColor = (colors[4])
+                self.circles.view.backgroundColor = (colors[4])
+                self.picker.view.backgroundColor = (colors[4])
+            }
+           
         }
     }
 }
